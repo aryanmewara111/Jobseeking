dbd67f1f7097cb5666ea5768884ba097f46dcebb
aryanmewara111 <117459932+aryanmewara111@users.noreply.github.com>
2024-03-25 17:24:03 +0530

    second commit

--- frontend/src/components/Application/MyApplications.jsx
+++ frontend/src/components/Application/MyApplications.jsx
@@ -45,7 +45,7 @@ const MyApplications = () => {
   const deleteApplication = (id) => {
     try {
       axios
-        .delete(`http://localhost:4000/api/v1/application/delete/${id}`, {
+        .delete(`${FRONTEND_URL}/api/v1/application/delete/${id}`, {
           withCredentials: true,
         })
         .then((res) => {
